e322ab283126305a6168fd1419569ffda47b92d1
Jubair <zub1984.kn@gmail.com>
2021-11-03 01:09:46 +0300

    updated for OpenTracing

--- backend/backend.py
+++ backend/backend.py
@@ -1,9 +1,23 @@
 from random import randint
 from time import sleep
 
-from flask import request
 from flask import Flask
+from flask import request
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
+
 app = Flask(__name__)
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 counter_value = 1
 
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,7 +1,20 @@
 import os
 import requests
 from flask import Flask
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
 app = Flask(__name__)
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 def get_counter(counter_endpoint):
     counter_response = requests.get(counter_endpoint)
